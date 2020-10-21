74e45b7ca62912d9332944bd33ec9e4547b64046
SwethaRedd <swethareddy.nadipi@gmail.com>
2020-10-21 17:59:41 -0400

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
     // The API(cloud function) URL
-    baseURL: 'https://us-central1-clone-amazon.cloudfunctions.net/api'
-    // baseURL: 'http://localhost:5001/clone-amazon/us-central1/api' // localhost : The API(cloud function) URL - local API endpoint
+    // baseURL: 'https://us-central1-clone-amazon.cloudfunctions.net/api'
+    baseURL: 'http://localhost:5001/clone-amazon/us-central1/api' // localhost : The API(cloud function) URL - local API endpoint
 });
 
 export default instance;
